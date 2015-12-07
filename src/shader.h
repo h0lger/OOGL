69e7254cf1fbe9130bbaaacef03452a2bb700046
david <audidave@gmail.com>
2015-12-07 21:01:14 +0100

    Begin of OO model

---
+++ src/shader.h
@@ -0,0 +1,34 @@
+#ifndef SHADER_INC_H
+#define SHADER_INC_H
+#include <string>
+#include <GL/gl.h>
+#include "transform.h"
+
+class Shader
+{
+public:
+    Shader(const std::string& fileName);
+
+    void Bind();
+    void Update(const Transform& transform, const Camera& camera);
+
+    virtual ~Shader();
+
+private:
+    static const unsigned int NUM_SHADERS = 2;
+    static const unsigned int NUM_UNIFORMS = 3;
+    void operator=(const Shader& shader) {}
+    Shader(const Shader& shader);
+
+    std::string LoadShader(const std::string& fileName);
+    void CheckForErrors(GLuint shader, GLuint flag, bool isProgram, const std::string& errMsg);
+    GLuint CreateShader(const std::string& text, unsigned int type);
+
+    GLuint _program;
+    GLuint _shaders[NUM_SHADERS];
+    GLuint _uniforms[NUM_UNIFORMS];
+
+}
+
+
+#endif // SHADER_INC_H
---
+++ src/transform.h
@@ -0,0 +1,56 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glx.h>
+#include <GL/glext.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include "camera.h"
+
+struct Transform
+{
+public:
+    Transform(const glm::vec3& pos = glm::vec3(), const glm::vec3& rot = glm::vec3(), const glm::vec3& scale = glm::vec3(1.0f, 1.0f, 1.0f))
+    {
+        this->_pos = pos;
+        this->_rot = rot;
+        this->_scale = scale;
+    }
+
+    inline glm::mat4 GetModel() const
+    {
+        glm::mat4 posMat = glm::translate(pos);
+        glm::mat4 scaleMat = glm::scale(scale);
+        glm::mat4 rotX = glm::rotate(rot.X, glm::vec3(1.0, 0.0, 0.0));
+        glm::mat4 rotY = glm::rotate(rot.Y, glm::vec3(0.0, 1.0, 0.0));
+        glm::mat4 rotZ = glm::rotate(rot.Z, glm::vec3(0.0, 0.0, 1.0));
+        glm::mat4 rotMat = rotX * rotY * rotZ;
+
+        return rotMat;
+    }
+
+    inline glm::mat4 GetMVP(const Camera& camera) const
+    {
+        glm::mat4 VP = camera.GetViewProjection();
+        glm::mat4 M = GetModel();
+
+        return VP * M;
+    }
+
+    inline glm::vec3* GetPos() { return &_pos; }
+    inline glm::vec3* GetRot() { return &_rot; }
+    inline glm::vec3* GetScale() { return &_scale; }
+
+    inline void SetPos(glm::vec3& pos) { this->_pos = pos; }
+    inline void SetRot(glm::vec3& rot) { this->_rot = rot; }
+    inline void SetScale(glm::vec3& scale) { this->_scale = scale; }
+
+private:
+    glm::vec3 _pos;
+    glm::vec3 _rot;
+    glm::vec3 _scale;
+}
+
+#endif // TRANSFORM_H
